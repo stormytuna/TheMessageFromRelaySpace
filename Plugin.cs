92353d667d026c9d77506607b2d77bbb2a90b913
stormytuna <stormytuna@outlook.com>
2026-08-23 20:21:50 +0100

    initial commit, the mod is technically usable, which is the best kind of usable

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace DSCR;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        harmony.PatchAll();
+    }
+}
+
